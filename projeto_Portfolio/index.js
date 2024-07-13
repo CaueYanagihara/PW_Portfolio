86ac3d0bd2bfffd8b069c651b6acd94fcd2c7491
Cauê Grande Yanagihara <134626371+CaueYanagihara@users.noreply.github.com>
2024-07-13 15:52:43 -0300

    Finalizado as funcionalidades

--- projeto_Portfolio/index.js
+++ projeto_Portfolio/index.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     verificarVarTema();
-    verificarVarLocalizacao();
 });
 
 function verificarVarTema(){
@@ -18,4 +17,54 @@ function alterarTema(){
     document.body.setAttribute("data-tema", novoTema);//ALTERA PARA O NOVO TEMA
     
     localStorage.setItem('tema', novoTema);//SALVA UMA VARIAVEL LOCAL NO NAVEGADOR
+}
+
+function copiar(botao){
+    const paragrafo = botao.previousElementSibling;
+    const informacao = paragrafo.textContent;
+
+    navigator.clipboard.writeText(informacao).then(()=>{
+        alert("Conteudo copiado!");
+    }).catch(error=>{
+        console.error("Erro ao copiar", error);
+        alert("Erro ao copiar.");
+    });
+}
+
+async function consultarRepositorios() {
+    const nomeUsuario = "cauegrandeyanagihara";
+
+    const listaRepositorios = document.getElementById("listaRepositorios");
+    listaRepositorios.innerText = '';
+
+    const status = document.getElementById("status");
+
+    if (!nomeUsuario) {
+        alert("Obrigatorio informar nome do usuario!");
+        return;
+    }
+
+    const url = `https://api.github.com/users/${nomeUsuario}/repos`;
+
+    status.innerText="Carregando...";
+    try {
+        const resposta = await fetch(url); //AGUARDA A RESPOSTA PARA DEPOIS EXECUTAR O RESTO DO CODIGO
+        //resposta.then(res=>{console.log(res)});       //-----EXECUTA O RESTO DO CODIGO ENQUANTO AGUARDA O FETCH ATRIBUIR UM VALOR A VARIAVEL.
+
+        if(!resposta.ok){
+            alert("Erro ao realizar a consulta.");
+            return;
+        }
+
+        const repositorios = await resposta.json();
+
+        repositorios.forEach(element => {
+            const itemLista = document.createElement('li');
+            itemLista.textContent = element.name;
+            listaRepositorios.appendChild(itemLista);
+        });
+
+        status.innerText = "";
+    } catch (error) {
+    }
 }
--- projeto_Portfolio/readme.txt
+++ projeto_Portfolio/readme.txt
@@ -1,3 +1,4 @@
-Bem-vindo ao projeto CurriculumVitae!
-Nele, será criado um site para hospedar meu currículo, utilizando HTML, CSS e JS.
-O desenvolvimento deste projeto é coordenado pelo professor Frank Willian Cardoso de Oliveira, do Instituto Federal do Paraná, como parte do aprendizado da disciplina de Programação Web.
+Bem-vindo ao projeto Portifolio!
+Nele será criado um site para hospedar meu currículo, utilizando HTML, CSS e JS. O desenvolvimento 
+deste projeto é coordenado pelo professor Frank Willian Cardoso de Oliveira do Instituto Federal do 
+Paraná, como parte do aprendizado da disciplina Programação Web.
